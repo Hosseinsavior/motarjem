8f7bef30ce44f46eb5435aa3a08de72d3516598c
Savi hoss <81635151+savior128@users.noreply.github.com>
2023-03-21 15:34:24 +0330

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -1,8 +1,8 @@
 import pymongo 
 import os
 
-DB_NAME = os.environ.get("DB_NAME","Hossein")
-DB_URL = os.environ.get("DB_URL","mongodb+srv://Savior:136707nafas@hossein.porr3rf.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DB_NAME","Hoss")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://Saviororg:zNiunxU9pI8pNHbd@cluster0.tmqkdun.mongodb.net/?retryWrites=true&w=majority")
 mongo = pymongo.MongoClient(DB_URL)
 db = mongo[DB_NAME]
 dbcol = db["USER"]
